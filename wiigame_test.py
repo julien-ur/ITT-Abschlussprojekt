0f3322628cc56a4b0e49405005ab6af39ace3299
Julien Wachter (Laptop) <julien.wachter@stud.uni-regensburg.de>
2017-07-22 23:31:07 +0200

    Updated: #11 succesfully removed most sorting errors with weird magic
    but hey, it works! :) probably needs some refactoring later

--- wiigame_test.py
+++ wiigame_test.py
@@ -178,14 +178,15 @@ class WiimoteNode(Node):
             ir_points.append(ir_point)
             ir_points.append(ir_point)
 
-        print(ir_points)
-        ir_points = self.sort_tracking_points(ir_points)
-        print(ir_points)
-
+        #print("raw", ir_points)
+        if(len(ir_points) == 4):
+            ir_points = self.sort_tracking_points(ir_points)
+        #print("ordered", ir_points)
+        '''
         if (len(ir_points) == 4):
-            drawing_point = self.calc_drawing_point(ir_points)
-            ir_points.append(drawing_point)
-
+        drawing_point = self.calc_drawing_point(ir_points)
+        ir_points.append(drawing_point)
+        '''
 
         return {'accelX': np.array([x_accel]), 'accelY': np.array([y_accel]), 'accelZ': np.array([z_accel]),
                 'ir_points': np.array(ir_points)}
@@ -193,45 +194,111 @@ class WiimoteNode(Node):
     def sort_tracking_points(self, ir_points):
         xmin, ymin = 100000, 100000
         xmax, ymax = 0, 0
-        xmin_point, xmax_point, ymin_point, ymax_point = [None for p in range(4)]
+        x_list_unordered, y_list_unordered = [], []
+        xmin_point, xmax_point, ymin_point, ymax_point = [(-1, -1) for p in range(4)]
+
+        #ir_points = [(582, 477), (947, 311), (507, 314), (841, 143)]
+        print("raw", ir_points)
 
         for i in range(len(ir_points)):
             p = ir_points[i]
+
+            x_list_unordered.append(p[0])
+            y_list_unordered.append(p[1])
+
             if (p[0] < xmin):
                 xmin = p[0]
                 xmin_point = p
-            elif (p[0] > xmax):
+            if (p[0] > xmax):
                 xmax = p[0]
                 xmax_point = p
             if (p[1] < ymin):
                 ymin = p[1]
                 ymin_point = p
-            elif (p[1] > ymax):
+            if (p[1] > ymax):
                 ymax = p[1]
                 ymax_point = p
 
         xmin_ymin_dist = math.hypot(xmin_point[0] - ymin_point[0], xmin_point[1] - ymin_point[1])
         xmin_ymax_dist = math.hypot(xmin_point[0] - ymax_point[0], xmin_point[1] - ymax_point[1])
 
-        quadrantNum = 1
-        if (xmin_ymin_dist > xmin_ymax_dist):
-            quadrantNum = 2
+        quadrant_num = 0 if (xmin_ymin_dist < xmin_ymax_dist) else 1
 
-        print(quadrantNum)
+        if (quadrant_num == 0):
+            sorted_tracking_points = [ymin_point, xmax_point, ymax_point, xmin_point]
+        elif (quadrant_num == 1):
+            sorted_tracking_points = [xmin_point, ymin_point, xmax_point, ymax_point]
 
-        if (quadrantNum == 1):
-            p1 = ymin_point
-            p2 = xmax_point
-            p3 = ymax_point
-            p4 = xmin_point
-        elif (quadrantNum == 2):
-            p1 = xmin_point
-            p2 = ymin_point
-            p3 = xmax_point
-            p4 = ymax_point
+        print("sorted", sorted_tracking_points)
 
-        sorted_tracking_points = [p for p in [p1,p2,p3,p4] if p is not None]
+        sorted_tracking_points = self.remove_sorting_errors(sorted_tracking_points, ir_points, quadrant_num, x_list_unordered, y_list_unordered)
+
         return sorted_tracking_points
+
+    def remove_sorting_errors(self, sorted_tracking_points, ir_points, quadrant_num, x_list_unordered, y_list_unordered, recursion_counter=0):
+        x_list_asc = sorted(x_list_unordered)
+        y_list_asc = sorted(y_list_unordered)
+
+        point_order_dict_quadrant_1 = [['y', 'min'], ['x', 'max'], ['y', 'max'], ['x', 'min']]
+        point_order_dict_quadrant_2 = [['x', 'min'], ['y', 'min'], ['x', 'max'], ['y', 'max']]
+        point_order_dict_list = [point_order_dict_quadrant_1, point_order_dict_quadrant_2]
+
+        avoidance_dict_quadrant_1 = [['x', 'max'], ['y', 'max'], ['x', 'min'], ['y', 'min']]
+        avoidance_dict_quadrant_2 = [['y', 'max'], ['x', 'min'], ['y', 'min'], ['x', 'max']]
+        avoidance_dict_list = [avoidance_dict_quadrant_1, avoidance_dict_quadrant_2]
+
+        duplicate_points = self.list_duplicates(sorted_tracking_points)
+        for dp in duplicate_points:
+            indices = [i for i, sp in enumerate(sorted_tracking_points) if str(sp) == str(dp)]
+            faulty_point_index = -1
+            lowest_avoidance_rating = 1000
+
+            if (len(indices) > 2):
+                print("more than 2 boundary values are identical. " +
+                      "that shouldn't happen, something's terrible wrong here :(")
+                return
+
+            for i in indices:
+                p = sorted_tracking_points[i]
+                avoidance_axis = avoidance_dict_list[quadrant_num][i][0]
+                avoidance_rating = x_list_asc.index(p[0]) if (avoidance_axis == "x") else y_list_asc.index(p[1])
+                if (avoidance_dict_list[quadrant_num][i][1] == "max"):
+                    avoidance_rating = len(sorted_tracking_points) - avoidance_rating
+
+                if (avoidance_rating < lowest_avoidance_rating):
+                    lowest_avoidance_rating = avoidance_rating
+                    faulty_point_index = i
+
+            faulty_point = sorted_tracking_points[faulty_point_index]
+            order_axis = point_order_dict_list[quadrant_num][faulty_point_index][0]
+            boundary_type = point_order_dict_list[quadrant_num][faulty_point_index][1]
+            search_list = x_list_asc if (order_axis == "x") else y_list_asc
+            closest_similar_value = search_list[1] if (boundary_type == "min") else search_list[len(sorted_tracking_points) - 2]
+            print(faulty_point)
+            print(order_axis, boundary_type)
+            print(closest_similar_value)
+
+            for p in ir_points:
+                order_axis_val = p[0] if (order_axis == "x") else p[1]
+                if (order_axis_val == closest_similar_value):
+                    sorted_tracking_points[faulty_point_index] = p
+                    print("corrected", sorted_tracking_points)
+
+        duplicate_points = self.list_duplicates(sorted_tracking_points)
+
+        if (len(duplicate_points) > 0 and recursion_counter < 4):
+            self.remove_sorting_errors(sorted_tracking_points, ir_points, quadrant_num, x_list_unordered, y_list_unordered, recursion_counter + 1)
+
+        return sorted_tracking_points
+
+    # Source: https://stackoverflow.com/a/9836685
+    def list_duplicates(self, seq):
+        seen = set()
+        seen_add = seen.add
+        # adds all elements it doesn't know yet to seen and all other to seen_twice
+        seen_twice = set(x for x in seq if x in seen or seen_add(x))
+        # turn the set into a list (as requested)
+        return list(seen_twice)
 
     def calc_drawing_point(self, ir_points):
         sx1 = ir_points[0][0]
