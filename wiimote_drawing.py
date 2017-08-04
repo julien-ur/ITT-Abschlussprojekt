a3b22118706608178ecc76e3917da4e19f262d62
julien <Julien.Wachter@stud.uni-regensburg.de>
2017-08-04 18:58:09 +0200

    updated drawing frequency, buffer weight

--- wiimote_drawing.py
+++ wiimote_drawing.py
@@ -33,7 +33,7 @@ class WiimoteDrawing:
         self.DEST_H = 1080
         self.IR_CAM_X = 1024
         self.IR_CAM_Y = 768
-        self.update_rate = 120
+        self.update_rate = 60
 
         self.wiimote = wiimote
         self._acc_vals = []
@@ -81,7 +81,7 @@ class WiimoteDrawing:
 
         # following filter only uses last point and current point
         # "trusting" the old point more than the new point by weight (0.3 means trusting new sample with 0.3
-        weight = 0.4
+        weight = 0.3
 
         if self._last_point is None:
             self._last_point = sample
